7755cfbe89e6a881746d93dea2d3882e33377d4f
steph5000 <51511634+steph5000@users.noreply.github.com>
2019-06-13 19:08:21 +0200

    javascript phase 4

--- solution.js
+++ solution.js
@@ -10,6 +10,15 @@ console.log("add half a teaspoon of" +  "" + ingrdients[2]);
 console.log("soak the" + "" + ingrdients[0] + "" + "in the egg mixture");
 console.log("put the" + "" + ingredients[0] + "" + "on the pan and turn both sides");
 console.log("remove from pan when golden and serve");
+
+
 //solution 2
 var books =[{title: "The Hobbit", author: "J.R.R tolken", alreadyRead: true,}, {title: "The lord of the rings", author: "J.R.R Tolken", alreadyRead: true,}, {title: "Harry Porter", author: "J.K Rowling", alreadyRead=false}];
-books.foreach(function(book) {
+books.foreach(function(book) 
+              { function book(value, index, array) { 
+  console.log(title + "" + author) 
+  { 
+  if (alreadyRead == true) { (console.log("you have aready read it") }
+                              else { console.log("you are still to read it") }
+  }
+  
