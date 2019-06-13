a63a865864a3645a72ae0878a3720710dc0abadc
steph5000 <51511634+steph5000@users.noreply.github.com>
2019-06-13 17:54:45 +0200

    recipe

--- solution.js
+++ solution.js
@@ -1,3 +1,13 @@
 // your solution should go here
 // for demo purposes use the console for any out you will need.
-var recipe = {title: "french toast", servings: 1, ingredients: ["bread", "one egg", "suger", "salt" "cooking oil"] };
+// solution 1
+
+var recipe = {title: "french toast", servings: 1, ingredients: ["slice of bread", "one egg", "suger", "salt" "cooking oil"] };
+console.log("mole serves:" + "" + recipe.servings + "" + " slice of french toast);
+console.log("heat the" +"" + ingredients[4] + "" + "on a pan");
+console.log("mix the" + "" + ingredients[1] + "" + "with a teaspoon of" + "" + ingredients[3]);
+console.log("add half a teaspoon of" +  "" + ingrdients[2]);
+console.log("soak the" + "" + ingrdients[0] + "" + "in the egg mixture");
+console.log("put the" + "" + ingredients[0] + "" + "on the pan and turn both sides");
+console.log("remove from pan when golden and serve");
+
