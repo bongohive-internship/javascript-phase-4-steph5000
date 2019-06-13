63b6e427537365d6f8bca992100043d3977f1cc2
steph5000 <51511634+steph5000@users.noreply.github.com>
2019-06-13 17:36:29 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,2 +1,3 @@
 // your solution should go here
 // for demo purposes use the console for any out you will need.
+var recipe = {title: "french toast", servings: 1, ingredients: ["bread", "one egg", "suger", "salt" "cooking oil"] };
